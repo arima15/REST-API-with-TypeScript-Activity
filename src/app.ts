60bb863d822564c4fbdd2692ed336295ef8722f4
Your Name <your.email@example.com>
2025-02-02 01:44:02 +0800

    final changes to REST API activity

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,28 @@
-import express from "express";
-import * as dotenv from "dotenv";
-import cors from "cors";
-import helmet from "helmet";
-import { userRouter } from "./users/users.routes";
+import express from "express"
+import * as dotenv from "dotenv"
+import cors from "cors"
+import helmet from "helmet"
+import { userRouter } from "./users/users.routes"
+import { productRouter } from "./products/product.routes"
 
 dotenv.config();
 
 if (!process.env.PORT) {
-    console.log('No port value specified...');
+    console.log('No port value specified...')
 }
 
-const PORT = parseInt(process.env.PORT as string, 10);
+const PORT = parseInt(process.env.PORT as string, 10)
 
-const app = express();
+const app = express()
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use(helmet());
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+app.use(helmet())
 
-app.use('/', userRouter);
+app.use('/', userRouter)
+app.use('/', productRouter)
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-});
+    console.log(`Server is listening on port ${PORT}`)
+})
